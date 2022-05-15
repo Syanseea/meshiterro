b082f135b86e7e976bab4d89359fe047bb11a38d
EC2 Default User <ec2-user@ip-172-31-44-54.ec2.internal>
2022-05-15 09:29:30 +0000

    [start]4-22

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: 'homes#top'
   get 'homes/about' => 'homes#about',as:'about'
+  
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
 end
--- db/migrate/20220514090745_create_post_comments.rb
+++ db/migrate/20220514090745_create_post_comments.rb
@@ -3,8 +3,7 @@ class CreatePostComments < ActiveRecord::Migration[6.1]
     create_table :post_comments do |t|
       t.text :comment
       t.integer :user_id
-      t.string :post_integer_id
-      t.string :integer
+      t.integer :post_image_id
 
       t.timestamps
     end
